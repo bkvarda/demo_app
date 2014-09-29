ac03e0aa1fcd7459b9340cfa9605dd55bc0ab36c
Brandon Kvarda <bjkvarda@gmail.com>
2014-09-28 18:41:15 -0700

    Finish demo app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+DemoApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -35,3 +35,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
